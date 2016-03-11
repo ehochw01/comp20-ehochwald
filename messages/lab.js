39d909ca26632aa9be4a19ba0a798da328efd5f7
eric_hochwald <hochwalderic@gmail.com>
2016-03-10 23:39:54 -0500

    Parse undefined for some reason

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,20 @@
-// Your JavaScript goes here...
+function parse() {
+	request = new XMLHttpRequest();
+
+	//makes request
+	request.open("GET", "data.json", true);
+
+	//Handles the response
+	request.onreadystatechange = function () {
+		if (request.readyState == 4 && request.status == 200) {
+			data = request.responseText;
+			messageData = JSON.parse(data);
+			message = document.getElementById("messages");
+			for (i = 0; i < messageData.length; i++) {
+				console.log("HI");
+				message.innerHTML = "<p>messageData[i].content + " " + messageData[i].username</p>";
+			}
+		}
+	};
+	request.send(null);
+}
